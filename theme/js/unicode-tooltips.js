2e13041a1778a99abd9ceca6f15c3565ebf73caa
Richard Harris <richardjharris@gmail.com>
2013-09-14 19:00:12 +0100

    My blog

---
+++ theme/js/unicode-tooltips.js
@@ -0,0 +1,30 @@
+$(function () {
+    $('.unichar').each(add_tooltip);
+});
+
+function add_tooltip() {
+    var element = $(this);
+    element.simpletip({
+        fixed: true,
+        position: 'bottom',
+        onBeforeShow: function() {
+            element.removeAttr('title');
+            var html = generate_tooltip_html(element);
+            this.update(html);
+        },
+    });
+}
+
+function generate_tooltip_html(el) {
+    var name = el.attr('data-name')
+    var code = el.attr('data-code');
+    var chr = el.contents().filter(function() { return this.nodeType == 3 }).text();
+
+    return '<span class="unicharbox"><bdo dir="ltr">' + chr + '</bdo></span>'
+        + ' <a href="http://www.fileformat.info/info/unicode/char/' + code + '">' + code + '</a>'
+        + ' ' + _encode(name);
+}
+
+function _encode(str) {
+    return $('<div/>').text(str).html();
+}
